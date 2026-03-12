b234137df733967803b73e5729906e5c0189b599
SGPscripts <260159011+SGPscripts@users.noreply.github.com>
2026-03-11 23:57:26 -0300

    Update Bear alpha skin changer.lua

--- Bear alpha skin changer.lua
+++ Bear alpha skin changer.lua
@@ -5,16 +5,55 @@ local Window = Rayfield:CreateWindow({
    Name = "Bear Skin Changer",
    LoadingTitle = "Cargando...",
    LoadingSubtitle = "test",
-   ConfigurationSaving = {
-      Enabled = false
-   }
+   ConfigurationSaving = {Enabled = false}
 })
 
 local Tab = Window:CreateTab("Skin Changer")
+local SkinsTab = Window:CreateTab("Skins")
 
 local IdleID = ""
 local Walk1ID = ""
 local Walk2ID = ""
+
+-- función para aplicar skin
+local function ApplySkin()
+
+   local player = game.Players.LocalPlayer
+   local char = player.Character
+   if not char then return end
+
+   local humanoid = char:FindFirstChildOfClass("Humanoid")
+   if not humanoid then return end
+
+   local sprite
+
+   for _,v in pairs(char:GetDescendants()) do
+      if v:IsA("ImageLabel") then
+         sprite = v
+         break
+      end
+   end
+
+   if not sprite then return end
+
+   task.spawn(function()
+      while sprite and sprite.Parent do
+
+         if humanoid.MoveDirection.Magnitude > 0 then
+            sprite.Image = "rbxassetid://"..Walk1ID
+            task.wait(0.15)
+
+            sprite.Image = "rbxassetid://"..Walk2ID
+            task.wait(0.15)
+         else
+            sprite.Image = "rbxassetid://"..IdleID
+            task.wait(0.2)
+         end
+
+      end
+   end)
+
+end
 
 Tab:CreateInput({
    Name = "Idle ID",
@@ -46,45 +85,23 @@ Tab:CreateInput({
 Tab:CreateButton({
    Name = "Apply Skin",
    Callback = function()
+      ApplySkin()
+   end
+})
 
-      local player = game.Players.LocalPlayer
-      local char = player.Character
-      if not char then return end
+-- BOTÓN DE SKIN PREHECHA
 
-      local humanoid = char:FindFirstChildOfClass("Humanoid")
-      if not humanoid then return end
+SkinsTab:CreateButton({
+   Name = "Captain Bear",
+   Callback = function()
 
-      local sprite
+      IdleID = "92159161315680"
+      Walk1ID = "9344509375188"
+      Walk2ID = "103419316396073"
 
-      for _,v in pairs(char:GetDescendants()) do
-         if v:IsA("ImageLabel") then
-            sprite = v
-            break
-         end
-      end
+      ApplySkin()
 
-      if not sprite then return end
-
-      print("sprite encontrado")
-
-      task.spawn(function()
-         while sprite and sprite.Parent do
-
-            if humanoid.MoveDirection.Magnitude > 0 then
-               sprite.Image = "rbxassetid://"..Walk1ID
-               task.wait(0.15)
-
-               sprite.Image = "rbxassetid://"..Walk2ID
-               task.wait(0.15)
-            else
-               sprite.Image = "rbxassetid://"..IdleID
-               task.wait(0.2)
-            end
-
-         end
-      end)
-
-      print("skin aplicada :)")
+      print("Captain Bear aplicado")
 
    end
 })
