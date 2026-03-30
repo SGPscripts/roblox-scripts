dfc65005ff3806672e55b35a5aeb4ae51cf56bc4
SGPscripts <260159011+SGPscripts@users.noreply.github.com>
2026-03-29 22:37:09 -0300

    Update TSB HUB.lua

--- TSB HUB.lua
+++ TSB HUB.lua
@@ -1,12 +1,14 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "TSB HUB",
-   LoadingTitle = "SGP SCRIPTS",
-   LoadingSubtitle = "Hecho por SGP SCRIPTS",
+   Name = "FPS Hub",
+   LoadingTitle = "FPS Hub",
+   LoadingSubtitle = "by Santi & ChatGPT",
    ConfigurationSaving = {
       Enabled = false,
    }
 })
 
-local Tab = Window:CreateTab("Main", 4483362458)
+-- tabs
+local MainTab = Window:CreateTab("Main", 4483362458)
+local InfoTab = Window:CreateTab("Info", 4483362458)
