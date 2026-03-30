24141d916e2fdf697d97de4eccc3b81b7021c5d0
SGPscripts <260159011+SGPscripts@users.noreply.github.com>
2026-03-29 22:27:55 -0300

    Create TSB HUB.lua

---
+++ TSB HUB.lua
@@ -0,0 +1,12 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "TSB HUB",
+   LoadingTitle = "SGP SCRIPTS",
+   LoadingSubtitle = "Hecho por SGP SCRIPTS",
+   ConfigurationSaving = {
+      Enabled = false,
+   }
+})
+
+local Tab = Window:CreateTab("Main", 4483362458)
