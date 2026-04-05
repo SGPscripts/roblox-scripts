b85c5e33718100cce5f6b31c2262a2bf559f0f64
SGPscripts <260159011+SGPscripts@users.noreply.github.com>
2026-04-04 21:43:42 -0300

    Update and rename Vortexion HUB to Vortexion HUB.lua

---
+++ Vortexion HUB.lua
@@ -0,0 +1,11 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+	Name = "Vortexion",
+	LoadingTitle = "Vortexion Hub",
+	LoadingSubtitle = "Loading...",
+	ConfigurationSaving = {
+		Enabled = false
+	},
+	KeySystem = false
+})
