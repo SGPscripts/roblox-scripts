62593f029b9a759965692c580b0f8024fc70f5b3
SGPscripts <260159011+SGPscripts@users.noreply.github.com>
2026-04-04 21:59:29 -0300

    Update Vortexion HUB.lua

--- Vortexion HUB.lua
+++ Vortexion HUB.lua
@@ -11,3 +11,41 @@ local Window = Rayfield:CreateWindow({
 })
 ---- useful xD
 local UsefulTab = Window:CreateTab("Useful", 4483362458)
+
+local player = game.Players.LocalPlayer
+
+-- SPEED
+UsefulTab:CreateSlider({
+	Name = "WalkSpeed",
+	Range = {16, 200},
+	Increment = 1,
+	Suffix = "Speed",
+	CurrentValue = 16,
+	Callback = function(Value)
+		if player.Character and player.Character:FindFirstChild("Humanoid") then
+			player.Character.Humanoid.WalkSpeed = Value
+		end
+	end,
+})
+
+-- JUMP POWER
+UsefulTab:CreateSlider({
+	Name = "JumpPower",
+	Range = {50, 200},
+	Increment = 1,
+	Suffix = "Jump",
+	CurrentValue = 50,
+	Callback = function(Value)
+		if player.Character and player.Character:FindFirstChild("Humanoid") then
+			player.Character.Humanoid.JumpPower = Value
+		end
+	end,
+})
+
+-- INFINITE YIELD
+UsefulTab:CreateButton({
+	Name = "Load Infinite Yield",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+	end,
+})
