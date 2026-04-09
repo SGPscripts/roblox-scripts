b63a25265ff90d044c8318119b291bc4960b013d
SGPscripts <260159011+SGPscripts@users.noreply.github.com>
2026-04-09 19:55:48 -0300

    Create Free spins.lua

---
+++ Free spins.lua
@@ -0,0 +1,50 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Spin Farm",
+    LoadingTitle = "Cargando...",
+    ConfigurationSaving = {
+        Enabled = false
+    }
+})
+
+local Tab = Window:CreateTab("Main", 4483362458)
+
+local toggle = false
+
+Tab:CreateToggle({
+    Name = "FREE SPINS",
+    CurrentValue = false,
+    Callback = function(Value)
+        toggle = Value
+
+        if toggle then
+            task.spawn(function()
+                while toggle do
+                    local folder = workspace:FindFirstChild("WinParts")
+                    
+                    if folder then
+                        local parts = {}
+
+                        for _, v in pairs(folder:GetChildren()) do
+                            if v:IsA("BasePart") then
+                                table.insert(parts, v)
+                            end
+                        end
+
+                        if #parts > 0 then
+                            local randomPart = parts[math.random(1, #parts)]
+
+                            local player = game.Players.LocalPlayer
+                            local char = player.Character or player.CharacterAdded:Wait()
+
+                            char:MoveTo(randomPart.Position + Vector3.new(0,3,0))
+                        end
+                    end
+
+                    task.wait(0.5)
+                end
+            end)
+        end
+    end
+})
